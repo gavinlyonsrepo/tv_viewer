f96ada5645ee485cb21cbe7b9c2f3702628614b9
Gavin lyons <glyons66@hotmail.com>
2017-11-22 00:10:53 +0000

    Create repo

---
+++ tv_viewer/tv_viewer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""python script """
+#=========================HEADER=======================================
+# title             :
+# description       :
+# author            :Gavin Lyons
+# date              :21/08/2017
+# version           :
+# web               :https://github.com/gavinlyonsrepo/
+# mail              :glyons66@hotmail.com
+# python_version    :
+
+#==========================IMPORTS======================
+# Import the system modules needed to run 
+
+#my modules
+
+
+
+#=======================GLOBALS=========================
+
+
+#====================FUNCTION SECTION===============================
+
+
+#=====================MAIN===============================
+def test():
+    """ docstring """
+    print("hello world")
+    pass
+
+if __name__ == '__main__':
+    test()
+#=====================END===============================
